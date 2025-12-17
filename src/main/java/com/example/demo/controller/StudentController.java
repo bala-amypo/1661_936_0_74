b2ca13dd5239ff5154f9c02fa1651d217606e0bd
bala-amypo <bala@amypo.in>
2025-12-17 09:46:21 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,9 +1,11 @@
-0pa0...............................................................................................000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000ckage.com.example.demo.controller;
+package.com.example.demo.controller;
 import com.example.demo.entity.StudentEntity;
+import org.springframework.stereotype.Service;
 import com.example.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.sprigframework.web.bind.annotation.RestController;
-@RestController
+import org.springframework.web.bind.annotation.RestController;
+@Service
+@Override
 public class StudentController{
     @AutoWired StudentService ser;
 
