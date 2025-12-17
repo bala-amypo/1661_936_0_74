78c3cea446e64c1f808e2d7f9a0ac2f4ebb62952
bala-amypo <bala@amypo.in>
2025-12-17 09:11:21 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
@@ -1,8 +1,18 @@
 package com.example.demo.service.Impl;
+import org.springframework.bens.factory.annotation.AutoWired;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.service.StudentService;
-@Service
-public class StudentServiceImpl implements S{
+import com.example.demo.entity.StudentEntity;
 
+
+@Service
+public class StudentServiceImpl implements StudentService{
+    @Autowired StudetRepository student;
+    //save()
+    //findall()
+    //findById()
+    //delete
+ public StudentEntity postData(StudentEntity stu){
+
+ }
 }
