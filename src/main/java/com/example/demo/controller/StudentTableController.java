a61b84d1dc477cedb428e071121fb42849d5f310
bala-amypo <bala@amypo.in>
2025-12-19 10:06:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/StudentTableController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+import com.example.demo.service.TimeStampService;
+import com.example.demo.entity.TimeStampEntity;
+import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+@RestController
+public class StudentTableController{
+@Autowired StudentTableService stamp;
+  
+    @PostMapping("/postforTimeStamp")
+    public StudentTableEntity sendData(@RequestBody StudentTableEntity Ent){
+        return stamp.postData(Ent);
+    }
+    }
