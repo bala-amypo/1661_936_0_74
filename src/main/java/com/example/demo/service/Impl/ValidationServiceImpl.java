afd8d68eab1ead6ebf6671cf941859cc5fcca756
bala-amypo <bala@amypo.in>
2025-12-19 04:37:18 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
@@ -18,17 +18,17 @@ public class ValidationServiceImpl implements ValidationService {
         return validation.save(val);
     }
 
-    @Override
-    public ValidationEntity updateData(int id, ValidationEntity entity) {
-        if (validation.existsById((long) id)) {
-            entity.setId((long) id);
-            return validation.save(entity);
-        }
+    // @Override
+    // public ValidationEntity updateData(int id, ValidationEntity entity) {
+    //     if (validation.existsById((long) id)) {
+    //         entity.setId((long) id);
+    //         return validation.save(entity);
+    //     }
 
-        return null;
-    }
+    //     return null;
+    // }
     @Override
-       public ValidationEntity getData(int id){
+       public ValidationEntity getData(long id){
          return validation.findById(id);
       } 
 }
