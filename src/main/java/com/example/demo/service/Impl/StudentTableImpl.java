6122bd97042c09b4917a88e61f7b2edbc82a7b81
bala-amypo <bala@amypo.in>
2025-12-19 10:27:42 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentTableImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentTableImpl.java
@@ -2,7 +2,7 @@
 package com.example.demo.service.Impl;
 
 import com.example.demo.entity.StudentTableEntity;
-import com.example.demo.exception.StudentTableException;
+
 import com.example.demo.repository.StudentTableRepository;
 import com.example.demo.service.StudentTableService;
 import org.springframework.beans.factory.annotation.Autowired;
