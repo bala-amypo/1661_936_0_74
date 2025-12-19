801a03e63167656768fc18ebfeba607511b000b3
bala-amypo <bala@amypo.in>
2025-12-19 08:09:14 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TimeStampImpl.java
+++ src/main/java/com/example/demo/service/Impl/TimeStampImpl.java
@@ -1,24 +1,4 @@
-package com.example.demo.service.Impl;
-import com.example.demo.exception.TimeStampException;
-import com.example.demo.entity.TimeStampEntity;
-import com.example.demo.repository.TimeStampRepository;
-import com.example.demo.service.
-;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class TimeStampImpl implements ValidationService {
-
-    @Autowired TimeStampRepository timestamp;
-
-    @Override
-    public TimeStampEntity postData(TimeStampEntity time) {
-        return timestamp.save(time);
-    }
-        
-}
 package com.example.demo.service.Impl;
 
 import com.example.demo.entity.TimeStampEntity;
@@ -29,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TimeStampServiceImpl implements TimeStampService {
+public class TimeStampImpl implements TimeStampService {
 
     @Autowired
     private TimeStampRepository timestamp;
--- src/main/java/com/example/demo/service/TimeStampService.java
+++ src/main/java/com/example/demo/service/TimeStampService.java
@@ -3,7 +3,7 @@ import com.example.demo.entity.TimeStampEntity;
 import java.util.List;
 public interface TimeStampService {
   
-TimeStampEntity postData(TimeStampEntity stu);
+TimeStampEntity postData(TimeStampEntity stamp);
 
 
 
