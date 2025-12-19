db4d2a7d848a4a3ab33c6fcc68780b93e6cdc0e2
bala-amypo <bala@amypo.in>
2025-12-19 10:21:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/StudentTableService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+import com.example.demo.entity.StudentTableEntity;
+import java.util.List;
+public interface StudentTableService {
+  
+StudentTableEntity postData(StudentTableEntity stabl);
+
+
+
+}
