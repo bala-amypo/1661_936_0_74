fce1e208f48e47a161f49da3796043ca9e2128b1
bala-amypo <bala@amypo.in>
2025-12-19 07:45:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/TimeStampController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+import com.example.demo.service.StudentService;
+import com.example.demo.entity.StudentEntity;
+import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+@RestController
+public class StudentController{
+@Autowired StudentService ser;
+  
+    @PostMapping("/post")
+    public StudentEntity sendData(@RequestBody StudentEntity ent){
+        return ser.postData(ent);
+    }
+    }
---
+++ src/main/java/com/example/demo/service/Impl/TimeStampImpl.java
@@ -0,0 +1,18 @@
+package com.example.demo.service.Impl;
+import com.example.demo.exception.TimeStampException;
+import com.example.demo.entity.TimeStampEntity;
+import com.example.demo.repository.TimeStampRepository;
+import com.example.demo.service.TimeStampService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ValidationServiceImpl implements ValidationService {
+
+    @Autowired ValidationRepository validation;
+
+    @Override
+    public ValidationEntity postData(ValidationEntity val) {
+        return validation.save(val);
+    }
