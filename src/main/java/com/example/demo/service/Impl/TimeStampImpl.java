a6e4f6d6a22e9ac8fd7a6f582c39ae0099490fea
bala-amypo <bala@amypo.in>
2025-12-19 08:06:14 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TimeStampImpl.java
+++ src/main/java/com/example/demo/service/Impl/TimeStampImpl.java
@@ -2,13 +2,14 @@ package com.example.demo.service.Impl;
 import com.example.demo.exception.TimeStampException;
 import com.example.demo.entity.TimeStampEntity;
 import com.example.demo.repository.TimeStampRepository;
-import com.example.demo.service.TimeStampService;
+import com.example.demo.service.
+;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TimeStampServiceImpl implements ValidationService {
+public class TimeStampImpl implements ValidationService {
 
     @Autowired TimeStampRepository timestamp;
 
@@ -17,4 +18,24 @@ public class TimeStampServiceImpl implements ValidationService {
         return timestamp.save(time);
     }
         
+}
+package com.example.demo.service.Impl;
+
+import com.example.demo.entity.TimeStampEntity;
+import com.example.demo.exception.TimeStampException;
+import com.example.demo.repository.TimeStampRepository;
+import com.example.demo.service.TimeStampService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TimeStampServiceImpl implements TimeStampService {
+
+    @Autowired
+    private TimeStampRepository timestamp;
+
+    @Override
+    public TimeStampEntity postData(TimeStampEntity time) {
+        return timestamp.save(time);
+    }
 }
