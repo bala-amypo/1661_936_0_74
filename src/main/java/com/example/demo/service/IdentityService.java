f132f7486ee4b15d0065f192bb0240ef4b1c6fe4
bala-amypo <bala@amypo.in>
2025-12-19 10:39:24 +0000

    User commit

--- src/main/java/com/example/demo/service/IdentityService.java
+++ src/main/java/com/example/demo/service/IdentityService.java
@@ -1,9 +1,9 @@
 package com.example.demo.service;
 import com.example.demo.entity.IdentityEntity;
 import java.util.List;
-public interface StudentTableService {
+public interface IdentityService {
   
-StudentTaeEntity postData(IdentityEntity itable);
+IdentityEntity postData(IdentityEntity itable);
 
 
 
