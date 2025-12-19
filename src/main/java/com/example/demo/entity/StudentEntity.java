d858112e15e50d3750d2063ffe12d35a7dfd7440
bala-amypo <bala@amypo.in>
2025-12-19 06:27:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -16,46 +16,4 @@ private String email;
 private String password;
 private Date createAt;
 
-public void setId(Integer id){
-    this.id=id;
-}
-public Integer getId(){
-     return id;
-}
-public String getUsername(){
-    return username;
-}
-public void setUsername(String username){
-    this.username=username;
-}
-public String getEmail(String Email){
-    return email;
-}
-public void setEmail(String email){
-    this.email=email;
-}
-
-public String getPassword(){
-    return password;
-}
-public void setPassword(String password){
-    this.password=password;
-}
-public Date getCreateAt(){
-    return createAt;
-}
-public void setCreateAt(Date createAt){
-    this.createAt=createAt;
-}
-public StudentEntity(Integer id,String username,String email,String password,Date createAt ){
-    this.id=id;
-    this.username=username;
-    this.email=email;
-    this.password=password;
-    this.createAt=createAt;
-
-}
-public StudentEntity(){
-
-}
 }
--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -28,55 +28,5 @@ public class ValidationEntity {
     @Max(30)
     private int age;
 
-    public ValidationEntity() {
-    }
-
     
-    public ValidationEntity(Long id, String username, String email, String password, int age) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.age = age;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
