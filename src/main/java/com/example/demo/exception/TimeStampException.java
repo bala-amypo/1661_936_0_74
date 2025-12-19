93adeaf6908d947a1ac50e0389e41903ee04c2e0
bala-amypo <bala@amypo.in>
2025-12-19 08:12:14 +0000

    User commit

--- src/main/java/com/example/demo/exception/TimeStampException.java
+++ src/main/java/com/example/demo/exception/TimeStampException.java
@@ -1,7 +1,6 @@
-
 package com.example.demo.exception;
 
-public class TimeStampException extends TimeStampException {
+public class TimeStampException extends RuntimeException {
 
     public TimeStampException(String message) {
         super(message);
