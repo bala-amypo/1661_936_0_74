45b9ee60f04e40cea112a4e6d0d0a165a5ddb1b6
bala-amypo <bala@amypo.in>
2025-12-19 04:53:41 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
@@ -1,5 +1,5 @@
 package com.example.demo.service.Impl;
-
+import com.example.demo.exception.ValidationException;
 import com.example.demo.entity.ValidationEntity;
 import com.example.demo.repository.ValidationRepository;
 import com.example.demo.service.ValidationService;
