8ad8ee68ece4a84ac5fc6ae0b32a6771c204e951
bala-amypo <bala@amypo.in>
2025-12-19 04:57:51 +0000

    User commit

--- src/main/java/com/example/demo/exception/ValidationException.java
+++ src/main/java/com/example/demo/exception/ValidationException.java
@@ -7,7 +7,7 @@ public class ValidationException extends RuntimeException{
 public ValidationException(String message){
 super(message);
 @Override
-       public ValidationEntity getData(Integer id){
+       public ValidationEntity getData(Long id){
                 return validation.findById(id);
                       
                             } 
